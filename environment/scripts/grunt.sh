bd9d768219ac24d627f7353f671212d007c7db39
vagrant <vagrant@precise64.(none)>
2014-05-23 14:37:00 +0000

    remove default grunt task

--- environment/scripts/grunt.sh
+++ environment/scripts/grunt.sh
@@ -7,10 +7,16 @@ cd /vagrant
 if [ -r "package.json" ]
 then
 	echo "Installing declared packages from package.json"
+	mkdir /node
+	cp /vagrant/package.json /node/package.json
+	cd /node
 	npm install --save-dev --no-bin-links
+	ln -s /node/node_modules /vagrant/node_modules
+	cd /vagrant
+
+
 	echo "Running grunt default (with --force if this is an empty project)"
 	grunt build --force
-	grunt
 else
 	echo "WARNING: There was no (readable) package.json. Create one and run 'npm install --save-dev --no-bin-links' to install manually."
 fi;
