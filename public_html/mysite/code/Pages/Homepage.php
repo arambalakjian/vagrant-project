8069574aa8e4f94ab7d06bfb9dd081762f9bf06b
Matthew Hadwen <matt@carboncrayon.com>
2014-09-19 15:32:22 +0100

    Prevent homepage url from being changed within cms

--- public_html/mysite/code/Pages/Homepage.php
+++ public_html/mysite/code/Pages/Homepage.php
@@ -1,8 +1,17 @@
 <?php
 
-class HomePage extends Page
-{
-	
+class HomePage extends Page {
+
+	public function canCreate($member = null) {
+		return HomePage::get()->first() ? false : parent::canCreate($member);
+	}
+
+	public function getCMSFields() {
+		$fields = parent::getCMSFields();
+		// don't let people accidentally change the home page URL
+		$fields->removeFieldFromTab('Root','URLSegment');
+		return $fields;
+	}
 }
 
 class HomePage_Controller extends Page_Controller
